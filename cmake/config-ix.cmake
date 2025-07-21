simonmcqueen/llvm-libcxx

Revert "[CMake] Use __libc_start_main rather than fopen when checking for C library"
This reverts commit r352341: it broke the build on macOS which doesn't
seem to provide __libc_start_main in its C library.

llvm-svn: 352411

libcxx/cmake/config-ix.cmake
@@ -7,7 +7,7 @@ if(WIN32 AND NOT MINGW)
   # let the default linking take care of that.
   set(LIBCXX_HAS_C_LIB NO)
 else()
-  check_library_exists(c __libc_start_main "" LIBCXX_HAS_C_LIB)
+  check_library_exists(c fopen "" LIBCXX_HAS_C_LIB)
 endif()
 
 if (NOT LIBCXX_USE_COMPILER_RT)

open-vela/nuttx_libs_libxx_libcxxabi

cmake/config-ix.cmake
@@ -2,7 +2,7 @@ include(CheckLibraryExists)
 include(CheckCCompilerFlag)
 include(CheckCXXCompilerFlag)
 
-check_library_exists(c __libc_start_main "" LIBCXXABI_HAS_C_LIB)
+check_library_exists(c fopen "" LIBCXXABI_HAS_C_LIB)
 if (NOT LIBCXXABI_USE_COMPILER_RT)
   check_library_exists(gcc_s __gcc_personality_v0 "" LIBCXXABI_HAS_GCC_S_LIB)
 endif ()